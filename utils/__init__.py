dbe9484a4acd69d4e71217179c97fc4c0dee23bc
MincoX <Minco.email>
2020-01-09 10:57:33 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s >>> %(filename)s:%(lineno)d >>> %(message)s')
+formatter = logging.Formatter('%(asctime)s, %(filename)s:%(lineno)d >>> %(levelname)s: %(message)s')
 
 log_file_handler = RotatingFileHandler(os.path.join(BASE_DIR, "logs/main.log"), maxBytes=1024 * 1024 * 1, backupCount=3)
 log_file_handler.setFormatter(formatter)
