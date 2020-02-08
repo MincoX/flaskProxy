93a3839578fbe9c316a4408421eeecb1e6cb7414
minco <minco>
2020-02-08 10:33:22 +0800

    update celery config

--- file_celery/celery_settings.py
+++ file_celery/celery_settings.py
@@ -7,12 +7,16 @@ result_backend = "redis://49.232.19.51:63791/2"  # 使用redis存储结果
 # broker_url = "redis://127.0.0.1:6379/1"  # 使用redis存储任务队列
 # result_backend = "redis://127.0.0.1:6379/2"  # 使用redis存储结果
 
+# 存储的结果被删除的时间（秒数，或者一个 timedelta 对象）
+result_expires = 60 * 60 * 24
+# 单个任务的运行时间限制，否则会被杀死（单位为：秒，windows 下无效）
+task_time_limit = 60 * 60 * 3
+
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
 timezone = "Asia/Shanghai"  # 时区设置
 worker_hijack_root_logger = False  # celery 默认开启自己的日志，可关闭自定义日志，不关闭自定义日志输出为空
-result_expires = 60 * 60 * 24  # 存储结果过期时间（默认1天）
 
 # 导入任务所在文件
 imports = [
